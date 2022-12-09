86440a5f6d865a5592799769be3ee970e6d65604
ForeverHaibara <69423537+ForeverHaibara@users.noreply.github.com>
2022-12-09 22:33:27 +0800

    new specialized algorithm for a structure

--- src/core/structsos/quintic.py
+++ src/core/structsos/quintic.py
@@ -11,7 +11,7 @@ def _sos_struct_quintic(poly, degree, coeff, recurrsion):
 
     if coeff((5,0,0)) == 0:
         if coeff((4,1,0)) == 0 and coeff((1,4,0)) >= 0:
-            if coeff((3,2,0)) > 0:
+            if coeff((3,2,0)) > 0 and coeff((2,3,0)) ** 2 <= 4 * coeff((1,4,0)) * coeff((3,2,0)):
                 # https://tieba.baidu.com/p/6472739202
                 u, x_, y_, z_, = sp.symbols('u'), coeff((1,4,0)) / coeff((3,2,0)), coeff((2,3,0)) / coeff((3,2,0)), coeff((3,1,1)) / coeff((3,2,0))
                 u_, y__, z__, w__ = None, y_, z_, coeff((2,2,1)) / coeff((3,2,0)) + (x_ + y_ + z_ + 1)
@@ -118,48 +118,13 @@ def _sos_struct_quintic(poly, degree, coeff, recurrsion):
                                                         f'a*b*c*(a*a+{(v*v + 2*v + 9)/4}*b*c)*(a*a+b*b+c*c-a*b-b*c-c*a)',
                                                         f'a*b*c*(a*a+{(v*v + 2*v + 9)/4}*b*c)*(a*b+b*c+c*a)']
 
-            elif coeff((3,2,0)) == 0:
-                0
-
-        elif coeff((1,4,0)) == 0 and coeff((4,1,0)) >= 0 and coeff((2,3,0)) > 0:
-            # https://tieba.baidu.com/p/6472739202
-            t = coeff((4,1,0)) / coeff((2,3,0))
-            if sp.ntheory.primetest.is_square(t.p) and sp.ntheory.primetest.is_square(t.q):
-                t, p_, q_ = coeff((2,3,0)), sp.sqrt(t.p), sp.sqrt(t.q)
-                x_ = p_ / q_
-                if coeff((3,2,0)) == -t * p_ / q_ * 2:
-                    v = sp.symbols('v')
-                    for root in sp.polys.roots(x_ * (v**3 - 3*v*v + 7*v - 13) + 4*(v + 1)).keys():
-                        if isinstance(root, sp.Rational) and root >= -1:
-                            v = root
-                            break
-                    else:
-                        v = None
-                    
-                    if v is not None:
-                        y_ = 4*(v**2-4*v+7)*(2*v**3-3*v**2+6*v-1)/(v**3-3*v**2+7*v-13)**2
-                        diff = coeff((3,1,1)) / t - (-2*x_*x_ + 2*x_ - y_)
-                        if diff >= 0:
-                            diff2 = coeff((2,2,1)) / t + diff - (x_*x_ + y_ - 1)
-                            if diff2 >= 0:
-                                if diff >= y_:
-                                    # use trivial method
-                                    diff -= y_
-                                    y = [sp.S(1), diff / 2, diff2]
-                                    y = [_ * t for _ in y]
-                                    names = [f'a*c*c*(a-{x_}*c+{x_-1}*b)^2', 'a*b*c*(a-b)^2', 'a^2*b^2*c']
-                                else:
-                                    u = (v*v - 2*v + 5) / 4
-                                    multipliers = [f'(a*a+{(v*v + 2*v + 9)/4}*b*c)']
-                                    y = [4/(v**3 - 3*v*v + 7*v - 13)**2, 8*(v*v - v + 1)*(v*v + 2*v + 13)/(v**3 - 3*v*v + 7*v - 13)**2,
-                                        diff, diff2]
-                                    y = [_ * t for _ in y]
-                                    names = [f'a*({-2*u*v-2*u+v**2-2*v+9}*a^2*c+{2*u*v+2*u-v**3+2*v**2-7*v+2}*a*c^2+{-2*v-2}*c^3'
-                                                    + f'+{v**2+2*v+1}*a^2*b+{-2*u*v**2+4*u*v-6*u+2*v**3-6*v**2+4*v}*a*b*c'
-                                                    + f'+{u*v**2-4*u*v+7*u+3*v**2+2*v-1}*c^2*b+{u*v**2-u-2*v-2}*a*b^2+{-v**3-v**2+5*v-7}*c*b^2)^2']
-                                    names += [f'a*b*c*(a*a-c*c+{u}*(a*c-a*b)+{v}*(b*c-a*c))^2',
-                                                f'a*b*c*(a*a+{(v*v + 2*v + 9)/4}*b*c)*(a*a+b*b+c*c-a*b-b*c-c*a)',
-                                                f'a*b*c*(a*a+{(v*v + 2*v + 9)/4}*b*c)*(a*b+b*c+c*a)']
+        elif coeff((1,4,0)) == 0 and coeff((4,1,0)) >= 0:
+            # reflect the polynomial
+            def new_coeff(c):
+                return coeff((c[0], c[2], c[1]))
+            multipliers, y, names = _sos_struct_quintic(None, 5, new_coeff, recurrsion)
+            if y is not None:
+                names = [_.translate({98: 99, 99: 98}) for _ in names]
 
         if y is None:
             # try hexagon
