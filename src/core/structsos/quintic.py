996d19342e3f986fc6b60c98451f66dd1684de5d
ForeverHaibara <69423537+ForeverHaibara@users.noreply.github.com>
2022-12-22 00:48:33 +0800

    add new structure of windmill

--- src/core/structsos/quintic.py
+++ src/core/structsos/quintic.py
@@ -118,6 +118,10 @@ def _sos_struct_quintic(poly, degree, coeff, recurrsion):
                                                         f'a*b*c*(a*a+{(v*v + 2*v + 9)/4}*b*c)*(a*a+b*b+c*c-a*b-b*c-c*a)',
                                                         f'a*b*c*(a*a+{(v*v + 2*v + 9)/4}*b*c)*(a*b+b*c+c*a)']
 
+            elif coeff((3,2,0)) == 0 and coeff((2,3,0)) >= 0 and coeff((1,4,0)) > 0:
+                multipliers, y, names = _sos_struct_quintic_uncentered(coeff)
+
+
         elif coeff((1,4,0)) == 0 and coeff((4,1,0)) >= 0:
             # reflect the polynomial
             def new_coeff(c):
@@ -138,7 +142,8 @@ def _sos_struct_quintic(poly, degree, coeff, recurrsion):
             b = coeff((4,1,0))
             if b >= -2 * a:
                 fpc = FastPositiveChecker()
-                name = '(a^2+b^2+c^2-a*b-b*c-c*a)*a*(a-b)*(a-c)'
+                # name = '(a^2+b^2+c^2-a*b-b*c-c*a)*a*(a-b)*(a-c)'
+                name = 'a*(a-b)^2*(a-c)^2'
                 poly2 = poly - a * sp.sympify(cycle_expansion(name))
                 fpc.setPoly(poly2)
                 if fpc.check() == 0:
@@ -154,4 +159,93 @@ def _sos_struct_quintic(poly, degree, coeff, recurrsion):
                         names = [name]
                         multipliers , y , names = _merge_sos_results(multipliers, y, names, recurrsion(poly2, 5))
                     
+    return multipliers, y, names
+
+
+def _sos_struct_quintic_uncentered(coeff):
+    """
+
+    Example
+    -------
+    s(2ab4+5a2b3-17a3bc+13a2b2c)
+
+    s(ab4+347/30a2b3-3833/230a3bc+475/69a2b2c)
+
+    s(ab4+0a2b3-19/4a3bc+9/2a2b2c)
+
+    s(2ab4+4a2b3-13a3bc+7a2b2c)
+    """
+
+    multipliers, y, names = [], None, None
+    u, v = sp.symbols('u v')
+    t = coeff((1,4,0))
+    r1, r2, r3, u_, v_ = coeff((2,3,0)) / t, coeff((3,1,1)) / t, coeff((2,2,1)) / t, None, None
+    r1_, r2_ = r1, r2
+    
+    if coeff((2,3,0)) + coeff((1,4,0)) + coeff((3,1,1)) + coeff((2,2,1)) == 0:
+        # v = (u^3 - u^2 + u + 1) / u
+        eq = (u**4 - u**3 - (1 + r1) * u - r1).as_poly(u)
+
+        for root in sp.polys.roots(eq, cubics = False, quartics = False).keys():
+            if isinstance(root, sp.Rational) and root > 0:
+                u_ = root
+                v_ = (u_**3 - u_**2 + u_ + 1) / u_
+                r1_ = r1
+                r2_ = -(3*u_**4 - 2*u_**3 + 3*u_ + 1)/(u_*(u_ + 1))
+                u_ = None
+        else:
+            for root in sp.polys.nroots(eq):
+                if root.is_real and root > 1:
+                    u_ = root
+                    break
+            if u_ is not None:
+                # approximate a rational number
+                u_numer = u_
+                for tol in (.3, .1, 3e-2, 3e-3, 3e-4, 3e-5, 3e-7, 3e-9):
+                    u_ = sp.Rational(*rationalize(u_numer - tol * 3, rounding = tol))
+                    if u_ > 0:
+                        r1_ = u_*(u_**3 - u_**2 - 1)/(u_ + 1) 
+                        if r1_ <= r1:
+                            r2_ = -(3*u_**4 - 2*u_**3 + 3*u_ + 1)/(u_*(u_ + 1))# + 2*(r1 - r1_)
+                            if r2_ <= r2:
+                                v_ = (u_**3 - u_**2 + u_ + 1) / u_
+                                break
+
+    if u_ is not None and v_ is not None:
+        u, v = u_, v_
+        w = (u + 1)*(2*u**2 - u*v - 3*u - v + 4)/((u**2 - u + 1)*(u**3 + u**2*v - u**2 - 3*u*v - u - v + 4))
+        d1 = u**2*v**2*w**2 + 2*u**2*v*w - 2*u*v*w**2 - 2*u*w - 2*u + w**2
+        factor1 = (u**4 + 3*u**3*v - 2*u**3 - u**2*v**2 - 5*u**2*v - u**2 - 2*u*v**2 + 5*u*v + 5*u - v**2 + 4*v - 6)
+        factor2 = (u**3 + u**2*v - u**2 - 3*u*v - u - v + 4)
+        a_ = -(u + 1) * (u*v - 1) * (2*u**2 - u*v - 3*u - v + 4) / factor1
+        phi = r1_ * (1/(u + a_)**2 - 1) + d1
+        rho = (u**2 - u*v - u + v**2 - v + 3)/(u + v - 1)
+        z = a_ / (u*v - 1)
+        
+        multipliers = [f'a^2+{phi}*a*b']
+        
+        y = [sp.S(1), 
+            r1_ * factor1**2 / ((u**2 - u + 1)**2 * factor2**2),
+            (u + v - 1) * (u**3 - u*u - u*v + u + 1) * (phi + 1) / (u*v - 1) / (u*u - 2*u - v + 2)
+            ]
+        y.append((r2_ + phi + 2*(u**3*w - u + v + w) - y[-1]) / 2)
+        y.append(1 if (r1 != r1_ or r2 != r2_) else 0)
+
+        y = [_ * t for _ in y]
+
+        names = [
+            f'a*({-u*v*w + w}*a^2*c + {u*u*v*w - u*w - u}*a*b^2 + {u**3*w - u*u*v*w - u*u*w + u*v*w + u*w - v*w + v}*a*b*c'
+           +f'+ b^3 + {-u**3*w + u - v - w}*b^2*c + {u**2*w + v*w - 1}*b*c^2)^2',
+            
+            f'a*(a^2*c + {u*v*z - z}*a*b^2 + {u**2*z - u*z + u - v**2*z + v*z - v}*a*b*c + {-u*v*z - u + z}*a*c^2 +'
+           +f'{-u**2*z - v*z - 1}*b^2*c + {u*z + v**2*z + v}*b*c^2)^2',
+
+            f'a*b*c*(a^2+b^2+c^2-{rho}*a*b-{rho}*b*c-{rho}*c*a)',
+
+            f'a*b*c*(a^2-b^2-{u}*a*c+{v}*b*c+{u-v}*a*b)^2',
+
+            f'a*c*(a-b)^2*({r1-r1_}*c+{(r2-r2_)/2}*b)*(a^2+b^2+c^2+{phi}*a*b+{phi}*b*c+{phi}*c*a)',
+        ]
+
+
     return multipliers, y, names
