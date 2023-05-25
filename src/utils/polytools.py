3d32c47dc1af37837ec966ca09bc6788609b659c
ForeverHaibara <69423537+ForeverHaibara@users.noreply.github.com>
2023-05-25 15:40:50 +0800

    refactor and optimize

--- src/utils/polytools.py
+++ src/utils/polytools.py
@@ -1,17 +1,29 @@
 import sympy as sp
 
-def deg(poly):
+def deg(poly: sp.polys.Poly):
     """
-    Return the degree of a polynomial.
+    Return the degree of a polynomial. We shall assume the polynomial is homogeneous.
+
+    Since the polynomial is homogeneous, we can simply return the sum of the exponents of the first monomial.
+    This avoids calling `monoms` and significantly improves the speed.
     """
-    return sum(poly.monoms()[0])
+    rep = poly.rep.rep
+    n = len(rep) + len(rep[0]) + len(rep[0][0]) - 3
+    # only exception is poly == 0 (n = -1)
+    return max(n, 0)
+
+    # too slow:
+    # return sum(poly.monoms()[0])
+
 
 
 def verify(y, polys, poly, tol: float = 1e-10) -> bool:
-    '''
+    """
     Verify whether the fraction approximation is valid
     by substracting the partial sums and checking whether the remainder is zero.
-    '''
+
+    Deprecated.
+    """
     try:
         for coeff, f in zip(y, polys):
             if coeff[0] != 0:
@@ -20,7 +32,7 @@ def verify(y, polys, poly, tol: float = 1e-10) -> bool:
                         poly = poly - sp.Rational(coeff[0] , coeff[1]) * f
                     else:
                         v = coeff[0] / coeff[1]
-                        coeff_dom = PreprocessText_GetDomain(str(v))
+                        coeff_dom = preprocessText_getdomain(str(v))
                         if coeff_dom != sp.QQ:
                             v = sp.polys.polytools.Poly(str(v)+'+a', domain=coeff_dom)\
                                  - sp.polys.polytools.Poly('a',domain=coeff_dom)
@@ -38,48 +50,89 @@ def verify(y, polys, poly, tol: float = 1e-10) -> bool:
 
 
 def verify_isstrict(func, root, tol=1e-9):
-    '''
+    """
     Verify whether a root is strict.
 
     Warning: Better get the function regularized beforehand.
-    '''
+
+    Deprecated.
+    """
     return abs(func(root)) < tol
 
 
-
-def verify_hom_cyclic(poly):
+def verify_hom_cyclic(poly, fast=True):
     """
     Check whether a polynomial is homogenous and 3-var-cyclic
 
+    Parameters
+    ----------
+    poly : sympy.Poly
+        The polynomial to be checked.
+    fast : bool
+        Whether to use a faster algorithm. Default is True. It is currently in experimental stage. 
+        The faster algorithm is based on direct operations on poly.rep.rep.
+
     Returns
-    -------
+    ----------
     is_hom : bool
         Whether the polynomial is homogenous.
     is_cyc : bool
         Whether the polynomial is 3-var-cyclic.
     """
-    n = sum(poly.monoms()[0])
+    if fast:
+        rep = poly.rep.rep
+        len0, len1, len2 = len(rep), len(rep[0]), len(rep[0][0])
+        n = len0 + len1 + len2 - 3
+        if n < 1:
+            return True, True
+        
+        t = n + 1
+        coeffs = [0] * t**2
 
-    if len(poly.args) != 4:
-        for monom in poly.monoms():
+        for i in range(len(rep)):
+            xi = rep[i]
+            if len(xi):
+                monom_i = len0 - i - 1
+                v = monom_i * t + len(xi)
+                rem = n - monom_i - len(xi)
+                for j in range(len(xi)):
+                    xj = xi[j]
+                    v -= 1 # this is a pointer to `coeffs`
+                    # monom_j = len(xi) - j - 1
+                    rem += 1 # rem = n - monom_i - monom_j
+                    if len(xj):
+                        monom_k = len(xj) - 1
+                        if monom_k != rem or any(xj[1:]):
+                            return False, False
+                        coeffs[v] = xj[0]
+
+    else:
+        # slow algorithm, abandoned
+        monoms = poly.monoms()
+        n = sum(monoms[0])
+
+        if len(poly.gens) != 3:
+            for monom in monoms:
+                if sum(monom) != n:
+                    return False, False
+            return True, False
+
+        t = n + 1
+        coeffs = [0] * t**2 # for i in range(t**2)]
+        for coeff, monom in zip(poly.coeffs(), monoms):
             if sum(monom) != n:
                 return False, False
-        return True, False
-
-    coeffs = {}
-    for coeff, monom in zip(poly.coeffs(), poly.monoms()):
-        if sum(monom) != n:
-            return False, False
-        coeffs[(monom[0], monom[1])] = coeff 
+            coeffs[monom[0]*t + monom[1]] = coeff
         
     for i in range((n-1)//3+1, n+1):
         # 0 <= k = n-i-j <= i
         for j in range(max(0,n-2*i), min(i+1,n-i+1)):
             # a^i * b^j * c^{n-i-j}
-            u = coeffs.get((i,j))
-            v = coeffs.get((j,n-i-j))
-            if u == v: # Nones are treated the same
-                w = coeffs.get((n-i-j,i))
+            k = n-i-j
+            u = coeffs[i*t + j]
+            v = coeffs[j*t + k]
+            if u == v:
+                w = coeffs[k*t + i]
                 if u == w:
                     continue 
             return True, False 
