cd8d7d161916a77e18f80947943027869972cc44
ForeverHaibara <69423537+ForeverHaibara@users.noreply.github.com>
2023-06-01 21:58:54 +0800

    Update __init__.py

--- src/utils/roots/__init__.py
+++ src/utils/roots/__init__.py
@@ -1,4 +1,16 @@
 from .rootsinfo import RootsInfo
+
 from .grid import GridRender
+
 from .findroot import findroot
+
 from .tangents import root_tangents
+
+from .rationalize import (
+    rationalize,
+    rationalize_array,
+    rationalize_bound,
+    rationalize_quadratic_curve,
+    square_perturbation,
+    cancel_denominator
+)
