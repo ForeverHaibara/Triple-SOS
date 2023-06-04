c779113d5e3dc37b8013be30971e0e1a834b25fa
ForeverHaibara <69423537+ForeverHaibara@users.noreply.github.com>
2023-06-04 14:13:33 +0800

    new GUI

--- web_main.py
+++ web_main.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 
 # from sum_of_square import *
+from src.utils.roots import RootTangent
+from src.utils.text_process import short_constant_parser
 from src.utils.expression.solution import SolutionSimple
 from src.gui.sos_manager import SOS_Manager
 
@@ -39,20 +41,11 @@ def preprocess():
     for i in range(n+1):
         for j in range(i+1):
             if monoms[t][0] == n - i and monoms[t][1] == i - j:
-                if isinstance(coeffs[t], sp.core.numbers.Float):
-                    txt = f'{round(float(coeffs[t]),4)}'
-                else:
-                    v = coeffs[t].as_numer_denom()
-                    txt = f'{v[0]}' + (f'/{v[1]}' if v[1] != 1 else '')
-                    if len(txt) > 10:
-                        txt = f'{round(float(coeffs[t]),4)}'
+                txt = short_constant_parser(coeffs[t])                
                 t += 1
             else:
                 txt = '0'
             txts.append(txt)
-            
-    # restore
-    monoms.pop()
 
     factor = ''
     print(request.get_json()['factor'])
@@ -69,15 +62,24 @@ def SumOfSquare():
     req = request.get_json()
     # app.SOS_Manager._roots_info['tangents'] = [tg
     #                     for tg in req['tangents'].split('\n') if len(tg) > 0]
+    app.SOS_Manager._roots_info.tangents = [
+        RootTangent(tg) for tg in req['tangents'].split('\n') if len(tg) > 0
+    ]
 
-    solution = app.SOS_Manager.sum_of_square()
-                                    #  use_structural_method=req['use_structural_method'])
+    if req['use_structural_method']:
+        method_order = ['StructuralSOS', 'LinearSOS']
+    else:
+        method_order = ['LinearSOS']
+
+    solution = app.SOS_Manager.sum_of_square(method_order = method_order)
+
     if solution is None:
         return jsonify(latex = '', txt = '', formatted = '', success = False)
 
     if isinstance(solution, SolutionSimple):
-        # remove the aligned environment
-        latex_ = '$$%s$$'%solution.str_latex[17:-15].replace('&','')
+        # # remove the aligned environment
+        # latex_ = '$$%s$$'%solution.str_latex[17:-15].replace('&','')
+        latex_ = solution.str_latex#.replace('aligned', 'align*')
 
     return jsonify(latex = latex_,
                     txt  = solution.str_txt,
