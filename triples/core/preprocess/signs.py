71a988089abb00d44d8240e73c09b0a63ceebc10
ForeverHaibara <1378855363@qq.com>
2025-10-20 18:21:45 +0800

    signs_sos new algorithms and tests

--- triples/core/preprocess/signs.py
+++ triples/core/preprocess/signs.py
@@ -1,14 +1,48 @@
-from typing import List, Dict, Tuple, Union, Optional
+from typing import List, Dict, Tuple, Union, Set, Optional
 
-from sympy import Expr, Poly, Rational, Integer, Mul, Symbol, true
+from sympy import Expr, Poly, Rational, Integer, Add, Mul, Symbol, true
 
 from ..problem import InequalityProblem
 from ...utils import CyclicExpr
 
-def sign_sos(expr: Expr, signs: Dict[Symbol, Tuple[int, Expr]]):
+def sign_sos(expr: Expr, signs: Dict[Symbol, Tuple[int, Expr]]) -> Optional[Expr]:
     """
     Very fast and simple nonnegativity check for a SymPy (commutative, real)
     expression instance given signs of symbols.
+
+    Parameters
+    ----------
+    expr: Expr
+        The expression to check.
+    signs: Dict[Symbol, Tuple[int, Expr]]
+        Signs of symbols stored in the format dict((symbol, (sign, expr))).
+        The sign is 1 if the symbol is non-negative and -1 if the symbol is
+        non-positive. The expr is the expression that the absolute value
+        of the symbol is replaced with. If the sign is 0, the symbol
+        is zero and the expr is the expression that the symbol is replaced
+        with.
+
+    Returns
+    -------
+    Optional[Expr]
+        The nonnegatie certificate of the input expression.
+        Returns None if the heuristic fails.
+
+    Examples
+    ---------
+    >>> from sympy.abc import a, b, c
+    >>> from sympy import Function
+    >>> F = Function('F')
+    >>> signs = {a: (1, F(a)), b: (1, b), c: (None, None)}
+    >>> sign_sos(a*b**3*c**2 + b + (a - b)**2 + 2/a, signs)
+    b**3*c**2*F(a) + b + (a - b)**2 + 2/F(a)
+    >>> sign_sos(c + a**2, signs) is None
+    True
+
+    Note that the function bases on heuristics,
+    and may fail on expressions that are nonnegative.
+    >>> sign_sos(a**12 + b**12 + c**12 - 3*a**4*b**4*c**4, signs) is None
+    True
     """
     def is_nonneg_pow(x: Expr) -> bool:
         if isinstance(x.exp, Rational) and (int(x.exp.numerator) % 2 == 0 or
@@ -89,11 +123,294 @@ def sign_sos(expr: Expr, signs: Dict[Symbol, Tuple[int, Expr]]):
         return sol[0]
 
 
+class SpecialHeap:
+    """
+    An efficient data structure storing a list of sets `[x[0], ..., x[n-1]]`.
+    It supports the following two operations:
+    1. `remove(a, b)`: call x[a].remove(b).
+    2. `pop()`: pop the set with minimum length.
+    """
+    def __init__(self, x: List[Set[int]]):
+        self.heap = []
+        self.pos = list(range(len(x))) # index of x[i] in heap
+
+        for xi, yi in zip(x, self.pos):
+            self.heap.append((xi, yi))
+        for i in range((len(self.heap) - 2) // 2, -1, -1):
+            self._bubble_down(i)
+
+    def __len__(self):
+        return len(self.heap)
+
+    def _bubble_up(self, i):
+        heap = self.heap
+        while i > 0:
+            parent = (i - 1) // 2
+            if len(heap[i][0]) < len(heap[parent][0]):
+                heap[i], heap[parent] = heap[parent], heap[i]
+                y_i = heap[i][1]
+                y_parent = heap[parent][1]
+                self.pos[y_i] = i
+                self.pos[y_parent] = parent
+                i = parent
+            else:
+                break
+
+    def _bubble_down(self, i):
+        heap = self.heap
+        n = len(heap)
+        while True:
+            left = 2 * i + 1
+            right = 2 * i + 2
+            smallest = i
+
+            if left < n and len(heap[left][0]) < len(heap[smallest][0]):
+                smallest = left
+            if right < n and len(heap[right][0]) < len(heap[smallest][0]):
+                smallest = right
+
+            if smallest != i:
+                heap[i], heap[smallest] = heap[smallest], heap[i]
+                y_i = heap[i][1]
+                y_smallest = heap[smallest][1]
+                self.pos[y_i] = i
+                self.pos[y_smallest] = smallest
+                i = smallest
+            else:
+                break
+
+    def remove(self, a: int, b: int):
+        """
+        Call x[a].remove(b)
+        """
+        i = self.pos[a]
+        if i >= len(self.heap): # ignore (i is not in the heap)
+            return
+        xi, yi = self.heap[i]
+        if yi != a: # ignore (i is not in the heap)
+            return
+        xi.remove(b)
+        self._bubble_up(i)
+
+    def pop(self) -> Tuple[Set[int], int]:
+        """
+        Get the element with the minimum length of x[i],
+        and remove it from the heap.
+        Returns (x[i], i).
+        """        
+        min_xi, min_yi = self.heap[0]
+    
+        if len(self.heap) == 1:
+            self.heap.pop()
+            return (min_xi, min_yi)
+
+        # fill the heap top with the last element, and bubble down
+        last_element = self.heap.pop()
+        self.heap[0] = last_element
+        self.pos[last_element[1]] = 0
+        self._bubble_down(0)
+
+        return (min_xi, min_yi)
+
+
+
+def _infer_separable_sign(poly: Poly, expr: Expr, s: int,
+        signs: Dict[int, Tuple[int, Expr]], cmp: int = 1) -> Tuple[int, int, Expr]:
+    """
+    Check whether `poly` implies the sign of the symbol `poly.gens[s]` by
+    rewriting `poly` as `r(...)*gens[s] + q(...) == expr (<>=) 0`.
+
+    When cmp == 1, the constraint is `poly == expr >= 0`.
+    When cmp == 0, the constraint is `poly == expr == 0`.
+
+    Currently require:
+    * `poly.gens[s]` is the only undetermined symbol in `signs`
+    * `poly` is linear in `poly.gens[s]`.
+
+    Returns `s, sgn, x` if inference succeeds, where
+    * `x = abs(gens[s])` if `cmp == 1`;
+    * `x = gens[s]` if `cmp == 0`.
+
+    Returns None if inference fails.
+
+    Examples
+    ---------
+    >>> from sympy.abc import a, b, c, x, y, z
+    >>> p1 = (c*a - 2 - c**2 - b).as_poly(a,b,c)
+    >>> signs = {1: (1, b), 2: (1, c)}
+    >>> _infer_separable_sign(p1, x, 0, signs, 1)
+    (0, 1, (b + c**2 + x + 2)/c)
+
+    Note that the `expr` stands for its absolute value if the sign is -1:
+    >>> p2 = (-(c + 1)*a - 2 - c**2 - b).as_poly(a,b,c)
+    >>> _infer_separable_sign(p2, x, 0, signs, 1)
+    (0, -1, (b + c**2 + x + 2)/(c + 1))
+
+    When inference fails, return None.
+    >>> p3 = (c*a + 2 - c**2 - b).as_poly(a,b,c)
+    >>> _infer_separable_sign(p3, x, 0, signs, 1) is None
+    True
+    """
+    if poly.degree(s) != 1:
+        return None
+    gens = poly.gens
+    nvars = len(gens)
+    def monom_sign(m, c, skip) -> Tuple[int, Expr]:
+        """
+        Returns (sgn(m*c), abs(m*c))
+        """
+        sgn = 1 if c > 0 else -1
+        absm = [-c if sgn == -1 else c]
+        for j in range(nvars):
+            mj = m[j]
+            if j == skip:
+                continue
+            absm.append(gens[j]**((mj//2)*2))
+            if mj % 2 == 1:
+                sj, e = signs[j]
+                sgn *= sj
+                absm.append(e)
+        return sgn, Mul(*absm)
+
+    qr = [[], []]
+    qr_sign = [-cmp, 0]
+
+    for monom, coeff in poly.terms():
+        k = 0 if monom[s] == 0 else 1
+        sgn, e = monom_sign(monom, coeff, s)
+        if qr_sign[k] == -sgn and sgn != 0: # inconsistent
+            return None
+        if qr_sign[k] == 0 and sgn != 0:
+            qr_sign[k] = sgn
+
+        # qr stores the absolute value of each term
+        qr[k].append(e)
+
+    q_sign, r_sign = qr_sign
+    if r_sign == 0: # division by zero
+        return None
+
+    absq = Add(*[x for x in qr[0]])
+    absr = Add(*[x for x in qr[1]])
+
+    if q_sign != 0:
+        # note that we return the absolute value of gens[s]
+        abss = (expr * (-q_sign) + absq) / absr
+    else:
+        abss = expr * r_sign / absr
+
+    return s, -q_sign * r_sign, abss
+
+
+def _get_signs_by_topological_order(ineq_constraints: Dict[Poly, Expr], eq_constraints: Dict[Poly, Expr],
+        signs: Dict[int, Tuple[Optional[int], Expr]]) -> Dict[int, Tuple[Optional[int], Expr]]:
+    """
+    Algorithm to infer the signs of symbols from constraints by
+    repeating the following process:
+    1. Extract the constraint with fewest undetermined symbols.
+    2. If the constraint has only one undetermined symbol,
+        try to infer the sign of the symbol from the constraint.
+    3. If the sign of the new symbol is determined,
+        remove the symbol from undetermined symbols of each constraint.
+
+    Parameters
+    ----------
+    ineq_constraints: Dict[Poly, Expr]
+        The inequalities constraints.
+    eq_constraints: Dict[Poly, Expr]
+        The equalities constraints.
+    signs: Dict[int, Tuple[Optional[int], Expr]]
+        The signs of symbols. Note that the function modifies
+        the `signs` in place.
+
+    Note
+    ----
+    * The function modifies `ineq_constraints` and `eq_constraints`
+    in place to remove constraints that are used or have no 
+    undetermined symbols.
+    * The function modifies the `signs` in place.
+
+    Returns
+    -------
+    signs: Dict[int, Tuple[Optional[int], Expr]]
+        The signs of symbols. It is the input `signs`.
+    """
+    if len(ineq_constraints):
+        gens = next(iter(ineq_constraints.keys())).gens
+    elif len(eq_constraints):
+        gens = next(iter(eq_constraints.keys())).gens
+    else:
+        return signs
+    nvars = len(gens)
+
+    def _is_separable(p: Poly) -> bool:
+        """
+        Check whether the polynomial can be written as x = q(...)/r(...)
+        where q and r is independent with the variable x.
+        This form helps infer the sign of the variable x.
+        """
+        if p.is_zero:
+            return False
+        return any(i == 1 for i in p.degree_list())
+
+    neighbors = {i: set() for i in range(nvars)}
+    cons = []
+    cons_fs = []
+    cons_cnt = 0
+    for cmp, constraints in ((1, ineq_constraints), (0, eq_constraints)):
+        for p, e in constraints.items():
+            if not _is_separable(p):
+                continue
+            pd = p.degree_list()
+            pfs = set([i for i in range(nvars) if pd[i] > 0 and signs[i][0] is None])
+            for s in pfs:
+                neighbors[s].add(cons_cnt)
+            cons.append((p, e, cmp))
+            cons_fs.append(pfs)
+            cons_cnt += 1
+
+    # maintain a heap so that we can access i with the minimum len(cons_fs[i])
+    heap = SpecialHeap(cons_fs)
+
+    def infer(i):
+        p, e, cmp = cons[i]
+        s = next(iter(cons_fs[i]))
+        return _infer_separable_sign(p, e, s, signs, cmp)
+
+    # print('Constraints', constraints)
+    while len(heap):
+        fs, i = heap.pop()
+        if len(fs) == 0:
+            continue
+        if len(fs) > 1:
+            # cannot eliminate symbols anymore
+            break
+        # if len(fs) == 1:
+        result = infer(i)
+        if result is None:
+            continue
+
+        s_ind, sgn, abs_s = result
+        signs[s_ind] = (sgn, abs_s)
+
+        if cons[i][2] == 1:
+            del ineq_constraints[cons[i][0]]
+        else:
+            del eq_constraints[cons[i][0]]
+
+        # print(f'Removing Neighbors[{s_ind}] = {neighbors[s_ind]}: {[cons[i] for i in neighbors[s_ind]]}')
+        for j in list(neighbors[s_ind]):
+            heap.remove(j, s_ind)
+            neighbors[s_ind] = {}
+
+    return signs
+
+
 def get_symbol_signs(problem: InequalityProblem) -> Dict[Symbol, Tuple[Optional[int], Expr]]:
     """
     Infer the signs of each symbol in the problem given inequality
-    and equality constraints. It can also be called by the class method
-    `InequalityProblem.get_symbol_signs()`.
+    and equality constraints heuristically. It can also be called
+    by the class method `InequalityProblem.get_symbol_signs()`.
 
     The inference is heuristic and incomplete.
 
@@ -107,73 +424,37 @@ def get_symbol_signs(problem: InequalityProblem) -> Dict[Symbol, Tuple[Optional[
 
         If sign == 1 or -1, Expr is an nonnegative expression equal to the ABSOLUTE VALUE
         of the symbol. If sign == 0, Expr is an expression of zero.
+
+    Examples
+    --------
+    >>> from sympy.abc import a, b, c, d, u, v, w, x, y, z
+    >>> pro = InequalityProblem(a, {c/2 - b - 2: x, b - a: y, a - 1: z, d + 2: w}, {})
+    >>> pro.get_symbol_signs()
+    {a: (1, z + 1), b: (1, y + z + 1), c: (1, 2*x + 2*y + 2*z + 6), d: (None, None)}
+
+    Note that the `expr` stands for its absolute value if the sign is -1,
+    so that it always look like an nonnegative expression:
+    >>> pro = InequalityProblem(a, {-a - 3: x, -b - a**2: y}, {c: v - 2})
+    >>> pro.get_symbol_signs()
+    {a: (-1, x + 3), b: (-1, a**2 + y), c: (0, v - 2)}
     """
 
-    eq_constraints, ineq_constraints = problem.eq_constraints, problem.ineq_constraints
+    eq_constraints0, ineq_constraints0 = problem.eq_constraints, problem.ineq_constraints
+    eq_constraints, ineq_constraints = {}, {}
 
-    fs0 = tuple(problem.free_symbols)
-    signs = {s: (None, None) for s in fs0}
-    for eq, expr in eq_constraints.items():
-        if not isinstance(eq, Poly):
-            eq = Poly(eq, *fs0)
-        fs = eq.gens
+    fs0 = tuple(sorted(list(problem.free_symbols), key=lambda x:x.name))
 
-        monoms = eq.monoms()
-        if len(monoms) == 1:
-            # x1**d1 * x2**d2 * ... == 0
-            lm = monoms[0]
-            c1 = eq.coeff_monomial(lm)
-            nnz = [i for i in range(len(lm)) if lm[i] != 0]
-            if len(nnz) == 1:
-                signs[fs[nnz[0]]] = (0, (expr / c1)**Rational(1, lm[nnz[0]]))
-        elif len(monoms) == 2:
-            # c1 * x1**d1 * x2**d2 * ... + c2* x1**e1 * x2**e2 * ... == expr >= 0
-            lm = monoms[0]
-            tm = monoms[1]
-            nnz1 = [i for i in range(len(lm)) if lm[i] != 0]
-            nnz2 = [i for i in range(len(tm)) if tm[i] != 0]
-            # if len(nnz1) == 0 or len(nnz2) == 0 or \
-            #     (len(nnz1) == 1 and len(nnz2) == 1 and nnz1[0] == nnz2[0]):
-            #     d1 = lm[nnz1[0]] if len(nnz1) else 0
-            #     d2 = tm[nnz2[0]] if len(nnz2) else 0
-            #     if (d1 - d2) % 2 == 1:
-            #         # e.g. x^5 - 3*x^2 = 0  => x = 0 or x = 3^(1/3)
-            #         c1 = eq.coeff_monomial(lm)
-            #         c2 = eq.coeff_monomial(tm)
-            #         sgn = bool(c1 > 0)^bool(c2 > 0)
-            #         sgn = 1 if sgn else -1
-            #         expr2 = sgn*(-sgn * c2 / c1)**Rational(1, d1 - d2)
-            #         signs[fs[nnz1[0]]] = (sgn, expr2)
+    # polylize and make a copy
+    for src, tar in ((eq_constraints0, eq_constraints), (ineq_constraints0, ineq_constraints)):
+        for key, val in src.items():
+            if (not isinstance(key, Poly)) or (not key.gens == fs0):
+                key = Poly(key, *fs0)
+            tar[key] = val
 
+    signs = {i: (None, None) for i in range(len(fs0))}
 
-    for ineq, expr in ineq_constraints.items():
-        if not isinstance(ineq, Poly):
-            ineq = Poly(ineq, *fs0)
-        fs = ineq.gens
+    # infer signs from constraints
+    signs = _get_signs_by_topological_order(ineq_constraints, eq_constraints, signs)
 
-        monoms = ineq.monoms()
-        c1 = 0
-        if len(monoms) == 1:
-            c1 = ineq.coeff_monomial(monoms[0])
-
-        # if len(monoms) == 2:
-        #     # c1 * t1 + c2 * t2 = expr >= 0
-        #     # equivalent to (c1/(c2*sgn))*(t1/t2) = expr/(c2*sgn*t2) - sgn >= 0
-        #     m1, m2 = monoms
-        #     c1 = ineq.coeff_monomial(m1)
-        #     c2 = ineq.coeff_monomial(m2)
-        #     monom_diff = tuple([m1[i] - m2[i] for i in range(len(m1))])
-        #     sgn = 1 if bool(c2 > 0) else -1
-        #     c1 = c1 / (sgn * c2)
-        #     expr = expr / (sgn * c2 * Mul(*[fs[i]**m2[i] for i in range(len(m2))])) - sgn
-        #     monoms = [monom_diff]
-        #     # now it goes to the len(monoms) == 1 case
-        if len(monoms) == 1:
-            # c1 * x1**d1 * x2**d2 * ... >= 0
-            lm = monoms[0]
-            odds = [i for i in range(len(lm)) if lm[i] % 2 == 1]  
-            if len(odds) == 1:
-                sgn = 1 if bool(c1 > 0) else -1
-                other = Mul(sgn*c1, *[fs[i]**lm[i] for i in range(len(lm)) if i != odds[0]])
-                signs[fs[odds[0]]] = (sgn, sgn*(expr / other)**Rational(1, lm[odds[0]]))
+    signs = {fs0[i]: signs.get(i, (None, None)) for i in range(len(fs0))}
     return signs
---
+++ triples/core/preprocess/tests/test_signs.py
@@ -0,0 +1,130 @@
+from sympy.abc import a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z
+from sympy import Function, Rational, fraction
+
+F, G = Function('F'), Function('G')
+
+import pytest
+
+from ...problem import InequalityProblem
+
+class InferSignProblems:
+    """
+    Each of the problem must return a tuple of (ineq_constraints, eq_constraints, signs)
+    """
+    @classmethod
+    def collect(cls):
+        return {k: getattr(cls, k) for k in dir(cls) if k.startswith('problem')}
+
+    @classmethod
+    def problem_basic_ineqs1(cls):
+        ineqs = {
+            a: F(a),
+            c*3: F(c),
+            3*g - 1: F(g),
+            d*-4/5: F(d),
+            -b: F(b),
+            e + 2: F(e),
+            -5*f + 3: F(f),
+            -2*h/3 - 4: F(h),
+        }
+        signs = {
+            a: (1, F(a)),
+            b: (-1, F(b)),
+            g: (1, F(g)/3 + Rational(1,3)),
+            c: (1, F(c)/3),
+            d: (-1, F(d)*5/4),
+            e: (None, None),
+            f: (None, None),
+            h: (-1, F(h)*3/2 + 6),
+        }
+        return ineqs, {}, signs
+
+    @classmethod
+    def problem_basic_eqs1(cls):
+        eqs = {
+            a: F(a),
+            c*3: F(c),
+            d*-4/5: F(d),
+            -b: F(b),
+            4*e/3 + 2: F(e),
+            -5*f + Rational(1,2): F(f),
+        }
+        signs = {
+            a: (0, F(a)),
+            b: (0, -F(b)),
+            c: (0, F(c)/3),
+            d: (0, F(d)*-5/4),
+            e: (-1, (2 - F(e))*3/4),
+            f: (1, (-F(f) + Rational(1,2))/5)
+        }
+        return {}, eqs, signs
+
+    @classmethod
+    def problem_relation1(cls):
+        ineqs = {
+            5*c - b: x,
+            a - 3: y,
+            b - 2*a: z,
+            d + b - a: u,
+            2*c - d*7: v,
+            e + 2: F(e),
+            -3*e - c**2*(a + 3): w,
+            (-a - 3 - 2*b**3)*g - 4*b - c**3/2 + f*2 - a*f**2: z,
+        }
+        eqs = {
+            2*f + 1: r,
+        }
+        b_ = z + 2*(y + 3)
+        c_ = (x + z + 2*(y + 3))/5
+        signs = {
+            a: (1, y + 3),
+            b: (1, b_),
+            c: (1, c_),
+            d: (None, None),
+            e: (-1, (w + c**2*(y + 6))/3),
+            f: (-1, (1 - r)/2),
+            g: (-1, (z + (y + 3)*f**2 - r + 1 + c_/2*c**2 + 4*b_)/(2*b_*b**2 + y + 6))
+        }
+        return ineqs, eqs, signs
+
+    @classmethod
+    def problem_relation2(cls):
+        ineqs = {
+            a*b - 2: x,
+            b + 3: y,
+            b*c - 4*a + 2: z,
+            -(b**2 + 2 - b*e)*d - 4*a - b: r
+        }
+        eqs = {
+            4*b - 1: u,
+            a*e + a**3 + b + 2: v,
+        }
+        a_ = (x + 2)/(u + 1)*4
+        b_ = (u + 1)/4
+        e_ = (-v + a_*a**2 + b_ + 2)/a_
+        signs = {
+            a: (1, a_),
+            b: (1, b_),
+            c: (None, None),
+            d: (-1, (r + 4*a_ + b_)/(b**2 + 2 + b_ * e_)),
+            e: (-1, e_),
+        }
+        return ineqs, eqs, signs
+
+
+@pytest.mark.parametrize("problem", InferSignProblems.collect().values(),
+    ids=InferSignProblems.collect().keys())
+def test_infer_signs(problem):
+    ineqs, eqs, signs0 = problem()
+    pro = InequalityProblem(Rational(0), ineqs, eqs)
+    signs1 = pro.get_symbol_signs()
+
+    assert set(signs1.keys()) == set(signs0.keys())
+
+    for key in signs1.keys():
+        sign0, expr0 = signs0[key]
+        sign1, expr1 = signs1[key]
+        assert sign0 == sign1, f"got signs[{key}] = {signs1[key]}, expected {signs0[key]}"
+        if sign0 is not None:
+            assert fraction((expr0 - expr1).together())[0].expand() == 0,\
+                f"got signs[{key}] = {signs1[key]}, expected {signs0[key]}"
